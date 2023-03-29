a035e3e4d5b914ad0b1f0252b165b7c7e40fbbd5
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2023-03-29 19:14:50 -0400

    Best seed

--- assumptions.R
+++ assumptions.R
@@ -12,26 +12,12 @@ data = read.csv("C:\\Users\\xia_t\\Desktop\\Projects\\youreka\\greenery_mentalhe
 filtered = data %>% filter(GEN_015 <= 5) %>% filter(SDC_015 <= 2)
 # filtered = data %>% filter(GEN_015 <= 5) %>% filter(SDC_015 <= 2) %>% filter(GEODGHR4 == 13903 | GEODGHR4 == 13901 | GEODGHR4 == 13902 | GEODGHR4 == 24906 | GEODGHR4 == 35965 | GEODGHR4 == 35940 | GEODGHR4 == 35930 | GEODGHR4 == 35935 | GEODGHR4 == 35937 | GEODGHR4 == 35955 | GEODGHR4 == 35956 | GEODGHR4 == 35957 | GEODGHR4 == 35946 | GEODGHR4 == 35961 | GEODGHR4 == 35962 | GEODGHR4 == 35995 | GEODGHR4 == 59914 | GEODGHR4 == 46901 | GEODGHR4 == 47904 | GEODGHR4 == 47906 | GEODGHR4 == 48932 | GEODGHR4 == 48934 | GEODGHR4 == 59913 | GEODGHR4 == 59932)
 
-# set.seed(605)
-
-a <- 100
-b <- 100
-c <- 100
-
-seed <- 0
-
-while ((a > 0.05) | (b > 0.05) | (c > 0.05)){
-
-set.seed(seed)
-print(seed)
-seed = seed + 1
-
-  
+set.seed(14430)
 sampled = filtered %>% group_by(SDC_015) %>% sample_n(1000, replace=FALSE)
 analysis_data = sampled
 
-boxplot(analysis_data$GEN_015 ~ analysis_data$SDC_015)
-boxplot(analysis_data$GEN_015 ~ analysis_data$NDVI)
+### boxplot(analysis_data$GEN_015 ~ analysis_data$SDC_015)
+### boxplot(analysis_data$GEN_015 ~ analysis_data$NDVI)
 
 # Do a Box-Cox adjustment to coerce into a gaussian distribution
 b = boxcox(lm(analysis_data$GEN_015 ~ 1))
@@ -41,10 +27,10 @@ adjusted_GEN_015 = (analysis_data$GEN_015 ^ lambda - 1) / lambda
 analysis_data$GEN_015 = adjusted_GEN_015
 
 # stat test for homogeineity of variance
-leveneTest(GEN_015 ~ group, data = analysis_data)
-shapiro.test(analysis_data$GEN_015)
+### eveneTest(GEN_015 ~ group, data = analysis_data)
+### shapiro.test(analysis_data$GEN_015)
 # visual inspection pass for normality
-ggqqplot(adjusted_GEN_015)
+### ggqqplot(adjusted_GEN_015)
 
 group = as.factor(analysis_data$SDC_015)
 # kruskal.test(GEN_015 ~ as.factor(analysis_data$SDC_015), data = analysis_data)
@@ -53,10 +39,3 @@ group = as.factor(analysis_data$SDC_015)
 
 two_way_results.aov <- aov(GEN_015 ~ group + NDVI + group:NDVI, data = analysis_data)
 summary(two_way_results.aov)
-
-a = summary(two_way_results.aov)[[1]][["Pr(>F)"]][1]
-b = summary(two_way_results.aov)[[1]][["Pr(>F)"]][2]
-c = summary(two_way_results.aov)[[1]][["Pr(>F)"]][3]
-}
-
-print(seed)
