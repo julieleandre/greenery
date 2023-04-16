aa685988f8f4ad679c6d37eb784295505edfb482
ThatAquarel <xia_tianyi@outlook.com>
2023-04-16 14:16:26 -0400

    Ordinal regression

--- tests.R
+++ tests.R
@@ -6,7 +6,9 @@ library(ggplot2)
 library(MASS)
 library(ggpubr)
 
-data <- read.csv("C:\\Users\\xia_t\\Desktop\\Projects\\youreka\\dataset\\all_subsetted_group.csv")
+# data <- read.csv("C:\\Users\\xia_t\\Desktop\\Projects\\youreka\\dataset\\all_subsetted_group.csv")
+data <- read.csv("/home/tianyi/src/youreka/dataset/all_subsetted_group.csv")
+
 
 # filter mental health and indigenous status
 filtered <- data %>%
@@ -23,11 +25,6 @@ filtered$NDVI <- (ndvi - min(ndvi)) / (max(ndvi) - min(ndvi))
 #  breaks = c(-Inf, 0.25, 0.5, 0.75, Inf),
 #  labels = c(1, 2, 3, 4)
 #)
-filtered$NDVI <- cut(
-  filtered$NDVI, 
-  breaks = c(-Inf, 0.5, Inf),
-  labels = c(1, 2)
-)
 
 # change SDC_015, NDVI and GEN_015 to factor
 filtered$SDC_015 <- as.factor(filtered$SDC_015)
@@ -35,10 +32,20 @@ filtered$NDVI <- as.factor(filtered$NDVI)
 filtered$GEN_015 <- as.factor(filtered$GEN_015)
 
 # sample data
-sampled <- filtered %>% slice_sample(n = 1000, replace=FALSE)
+sampled <- filtered %>% 
+  group_by(SDC_015) %>%
+  slice_sample(n = 500, replace=FALSE)
 
 # rename
 analysis_data <- sampled
 
-# chi2 test
-chisq.test(table(filtered$SDC_015, filtered$GEN_015))
+m <- polr(GEN_015 ~ SDC_015 + NDVI, data = analysis_data, Hess=TRUE)
+summary(m)
+
+(ctable <- coef(summary(m)))
+
+## calculate and store p values
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+
+## combined table
+(ctable <- cbind(ctable, "p value" = p))
